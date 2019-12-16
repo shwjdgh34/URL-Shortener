f431d86a08bf2122cd54f6c00c1399a6accb96d1
Jeongho Noh <nossi8128@gmail.com>
2019-12-16 21:49:20 +0900

    different baseURL depends on host url

--- app.js
+++ app.js
@@ -3,34 +3,38 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//reference : https://gist.github.com/gordonbrander/2230317
 const makeID = () => {
   return Math.random()
     .toString(36)
     .substr(2, 9);
 };
 const registerURL = (req, res) => {
+  const protocol = req.protocol;
+  const host = req.get('host');
   const reqURL = req.query.url;
   //const URLDB = JSON.parse(fs.readFileSync(`${__dirname}/db/URLDB.json`));
   fs.readFile(`${__dirname}/db/URLDB.json`, (err, data) => {
+    if (err) throw err;
     const URLDB = JSON.parse(data);
     // search DB wether the url exists already.
-    const dbURL = URLDB.find(el => el.url === reqURL);
+    const dbURLObj = URLDB.find(el => el.url === reqURL);
 
     // if not exist - create
-    if (!dbURL) {
+    if (!dbURLObj) {
       const newId = makeID();
-      const newURL = Object.assign({ id: newId, url: reqURL });
-      URLDB.push(newURL);
+      const newURLObj = Object.assign({ id: newId, url: reqURL });
+      URLDB.push(newURLObj);
       fs.writeFile(`${__dirname}/db/URLDB.json`, JSON.stringify(URLDB), err => {
         res.status(201).json({
-          url: `http://localhost:3000/${newURL.id}`
+          url: `${protocol}://${host}/${newURLObj.id}`
         });
       });
     }
     // if exist - return
     else {
       res.status(200).json({
-        url: `http://localhost:3000/${dbURL.id}`
+        url: `${protocol}://${host}/${dbURLObj.id}`
       });
     }
   });
@@ -39,11 +43,12 @@ const redirectToURL = (req, res) => {
   const id = req.params.id;
   fs.readFile(`${__dirname}/db/URLDB.json`, (err, data) => {
     if (err) throw err;
-    const db = JSON.parse(data);
-    const result = db.find(val => val.id === id);
+    const URLDB = JSON.parse(data);
+    const dbURLObj = URLDB.find(val => val.id === id);
 
-    if (result) {
-      res.redirect(301, result.url);
+    if (dbURLObj) {
+      res.redirect(301, dbURLObj.url);
+      //res.status(200).json(dbURLObj.url);
     } else {
       res.status(404);
     }
