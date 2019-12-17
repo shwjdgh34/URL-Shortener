36d4841a7535f8d43f08a085e052b600127bf277
Jeongho Noh <nossi8128@gmail.com>
2019-12-17 13:10:55 +0900

    refactoring callback to async/await

--- app.js
+++ app.js
@@ -3,28 +3,46 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 3000;
+// constructed promise for readFile() and writeFile()
+const readFilePro = file => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('Could not find that file 😥');
+      resolve(data);
+    });
+  });
+};
 
+const writeFilePro = file => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, err => {
+      if (err) reject('Could not write that file 😥');
+      resolve('success to write');
+    });
+  });
+};
 //reference : https://gist.github.com/gordonbrander/2230317
 const makeID = () => {
   return Math.random()
     .toString(36)
     .substr(2, 9);
 };
-const registerURL = (req, res) => {
+const registerURL = async (req, res) => {
   const protocol = req.protocol;
   const host = req.get('host');
   const reqURL = req.query.url;
   const baseURL = `${protocol}://${host}`;
   //const URLDB = JSON.parse(fs.readFileSync(`${__dirname}/db/URLDB.json`));
-  fs.readFile(`${__dirname}/db/URLDB.json`, (err, data) => {
-    if (err) throw err;
+  //try- catch
+  try {
+    const data = await readFilePro(`${__dirname}/db/URLDB.json`);
     const URLDB = JSON.parse(data);
     // search DB wether the url exists already.
     const dbURLObj = URLDB.find(el => el.url === reqURL);
-
     // if not exist - create
     if (!dbURLObj) {
       console.log(URLDB.length);
+      // TODO: make constraint fuction
       if (URLDB.length >= 10000) {
         return res.status(409).json({
           status: 'fail to save url due to full db'
@@ -33,10 +51,9 @@ const registerURL = (req, res) => {
       const newId = makeID();
       const newURLObj = Object.assign({ id: newId, url: reqURL });
       URLDB.push(newURLObj);
-      fs.writeFile(`${__dirname}/db/URLDB.json`, JSON.stringify(URLDB), err => {
-        res.status(201).json({
-          url: `${baseURL}/${newURLObj.id}`
-        });
+      await writeFilePro(`${__dirname}/db/URLDB.json`, JSON.stringify(URLDB));
+      res.status(201).json({
+        url: `${baseURL}/${newURLObj.id}`
       });
     }
     // if exist - return
@@ -45,7 +62,9 @@ const registerURL = (req, res) => {
         url: `${baseURL}/${dbURLObj.id}`
       });
     }
-  });
+  } catch (err) {
+    console.log(err);
+  }
 };
 const redirectToURL = (req, res) => {
   const id = req.params.id;
